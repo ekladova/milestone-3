f8f3b40a0e8f6d4f26008c6e38412ec1dab9baa7
Kate Kladova <ek.kladova@gmail.com>
2020-06-18 17:40:53 +0100

    Updates

---
+++ precache-manifest.5c86a312c19f85fc266a38ba8b61d76e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5bd1ce546ffea1b24edf9d8e59fc75d",
+    "url": "/milestone-3/index.html"
+  },
+  {
+    "revision": "a2cf0be22d79cefd32e1",
+    "url": "/milestone-3/static/css/main.f2bad147.chunk.css"
+  },
+  {
+    "revision": "17cc179941ea2302ad9e",
+    "url": "/milestone-3/static/js/2.e1607d2c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/milestone-3/static/js/2.e1607d2c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a2cf0be22d79cefd32e1",
+    "url": "/milestone-3/static/js/main.2a390eac.chunk.js"
+  },
+  {
+    "revision": "745b139ef808b1da72db",
+    "url": "/milestone-3/static/js/runtime-main.ca7caa97.js"
+  }
+]);
